169dc6bb7dbc8e7798027b80cc9193cbd03b6bec
Dzmitry Halavach <kotik1@protonmail.com>
2022-09-15 01:06:08 +0300

    fix: handling of sparse arrays, NaN, and context

--- arrayMapImplementation.js
+++ arrayMapImplementation.js
@@ -1,9 +1,11 @@
-//TODO fix sparse arrays bug and context handling
-
-Array.prototype.map = function (callback) {
-  let result = [];
+Array.prototype.map = function (callback, context) {
+  let result = new Array(this.length);
   for (let index = 0; index < this.length; index++) {
-    result.push(callback(this[index], index, this));
+    if (!(index in this)) {
+      result[index] = undefined;
+    } else if (!(index === void 0)) {
+      result[index] = callback.call(context, this[index], index, this);
+    }
   }
   return result;
 };
